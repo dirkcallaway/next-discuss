21117abbcb4102232b38401d027d741144e1367b
Matt Perroni <mattperroni@Matts-MacBook-Air.local>
2025-11-07 14:57:42 -0700

    Created createPost component and added validation on creating a post

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -1,6 +1,53 @@
 'use server';
 
-export async function createPost() {
+import type { Post }from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import { auth } from '@/auth';
+import { z } from 'zod';
+import paths from '@/paths';
 
-    // TODO: revalidate the topic show page after creating a post
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+
+interface CreatePostFormState {
+  errors: {
+    title?: string[],
+    content?: string[],
+    _form?: string[],
+  }
+}
+
+export async function createPost(
+  formState: CreatePostFormState,
+  formData: FormData
+): Promise<CreatePostFormState> {
+  const result = createPostSchema.safeParse({
+    title: formData.get('title'),
+    content: formData.get('content'),
+  });
+
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors
+    }
+  }
+
+  const session = await auth();
+  if (!session || !session.user) {
+    return {
+      errors: {
+        _form: ['You must be logged in to create a post.']
+      },
+    };
+  }
+
+  return {
+    errors: {}
+  };
+
+  // TODO: revalidate the topic show page after creating a post
 };
