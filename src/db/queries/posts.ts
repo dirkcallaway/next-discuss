1a3dd89c0acced846dd103a31e5887723e0e7b5c
Matt Perroni <mattperroni@Matts-MacBook-Air.local>
2025-11-18 19:13:17 -0700

    Fixed spacing

--- src/db/queries/posts.ts
+++ src/db/queries/posts.ts
@@ -2,30 +2,30 @@ import type { Post } from '@prisma/client';
 import { db } from '@/db';
 
 export type PostWithData = (
-    Post & {
-        topic: { slug: string };
-        user: { name: string | null };
-        _count: { comments: number };
-    }
+  Post & {
+    topic: { slug: string };
+    user: { name: string | null };
+    _count: { comments: number };
+  }
 )
 
 export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
-    return db.post.findMany({
-        where: {
-            topic: {
-                slug,
-            },
-        },
-        include: {
-            topic: {
-                select: { slug: true },
-            },
-            user: {
-                select: { name: true },
-            },
-            _count: {
-                select: { comments: true },
-            },
-        },
-    })
+  return db.post.findMany({
+    where: {
+      topic: {
+        slug,
+      },
+    },
+    include: {
+      topic: {
+        select: { slug: true },
+      },
+      user: {
+        select: { name: true },
+      },
+      _count: {
+        select: { comments: true },
+      },
+    },
+  })
 }
