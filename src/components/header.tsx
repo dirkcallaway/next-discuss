770a4a1e0e30203d429c544d88bbac3a33a11df8
Matt Perroni <mattperroni@Matts-MacBook-Air.local>
2025-12-09 18:21:06 -0700

    Added suspense around clientComponent

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,6 +7,7 @@ import {
 } from "@nextui-org/react";
 import HeaderAuth from '@/components/header-auth';
 import SearchInput from '@/components/search-input';
+import { Suspense } from "react";
 
 export default function Header() {
 
@@ -17,7 +18,9 @@ export default function Header() {
       </NavbarBrand>
       <NavbarContent justify='center'>
         <NavbarItem>
-          <SearchInput />
+          <Suspense>
+            <SearchInput />
+          </Suspense>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify='end'>
